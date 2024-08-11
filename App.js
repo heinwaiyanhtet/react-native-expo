dcebe4c4f685c8874deb41e8c0dd452a10b9fb59
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-08-11 23:15:13 +0630

    react native image and button

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Image } from 'react-native';
 
+// import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
 
 const PlaceHolderImage = require('./assets/me.jpg');
 
 export default function App() {
   return (
     <View style={styles.container}>
-       <View style={styles.imageContainer}>
-        <Image source={PlaceHolderImage} style={styles.image} />
+
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceHolderImage}/>
       </View>
+
+      <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" />
+          <Button label="Use this photo" />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -31,5 +41,9 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 18,
-  }
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
 });
